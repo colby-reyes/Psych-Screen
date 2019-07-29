dd5ca6c9f3a563877fedddff6b0b24c19313b32d
colby-reyes <colby.n.reyes@gmail.com>
2019-07-29 17:19:13 -0500

    updated working version: using dictionary to make function more concise

--- Setup/fndef.py
+++ Setup/fndef.py
@@ -7,43 +7,12 @@ class App:
         print("Initializing Psych-Screen app...")
 
 class Questionnaire:
-    def __init__(self, head, prompts, answers, scores):
+    def __init__(self, head, prompts, answers):
         self.head = head
         self.prompts = prompts
         self.answers = answers
-        self.scores = scores
+        #self.scores = scores
         #return super().__init__(prompt, answer)
-
-header="Over the past 2 weeks, how often have you been bothered by...\n"
-opts = ["0","1","2","3"]
-dispOpts = [
-    "0 - Not at all",
-    "1 - Several days",
-    "2 - More than half the days",
-    "3 - Nearly every day"
-]
-
-phq_prompts = [
-    Questionnaire(header, "Little interest or pleasure in doing things", dispOpts, opts),
-    Questionnaire(header, "Feeling down, depressed, or hopeless", dispOpts, opts),
-    Questionnaire(header, "Trouble falling asleep or staying asleep, or sleeping too much", dispOpts, opts),
-    Questionnaire(header, "Feeling tired or having little energy", dispOpts, opts),
-    Questionnaire(header, "Poor appetite or overeating", dispOpts, opts),
-    Questionnaire(header, "Feeling bad about yourself; feeling that you are a failure or that you have let yourself or your family down", dispOpts, opts),
-    Questionnaire(header, "Trouble concentrating on things (such as reading the newspaper or watching TV", dispOpts, opts),
-    Questionnaire(header, "Moving or speaking so slowly that other people could have noticed. Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual", dispOpts, opts),
-    Questionnaire(header, "Thoughts that you would be better off dead, or of hurting yourself", dispOpts, opts)
-]
-
-
-endOpts = [
-"0 - Not difficult at all",
-"1 - Somewhat difficult",
-"2 - Very difficult",
-"3 - Extremely difficult"
-]
-
-endQ = [Questionnaire(head="",prompts="If you checked off any problems, how difficult have these problems made it for you to do your work, take care of things at home, or get along with other people?",answers=endOpts,scores=opts)]
 
 
 def run_qstnr(q): 
@@ -51,21 +20,21 @@ def run_qstnr(q):
     i = 0
     while i < len(q):
         print(q[i].head + q[i].prompts)
-        for lin in q[i].answers:
+        ref = q[i].answers
+        for lin in ref.values():
             print(lin)
         ans = input("\n Answer:  ")
-        if ans in q[i].scores:
-            score += int(ans)
-            print("Your answer: {}".format(q[i].answers[int(ans)][4:]))
-            print("_"*50 + "\n")
-            i+=1
+        if ans == "Q":
+            break
         else:
-            print("Answer not recognized...please try again...")
-            print("_"*50 + "\n")
+            try:
+                ans = int(ans)
+                ans in ref.keys()
+                score += ans
+                print("Your answer: {}".format(ref[ans][4:]))
+                print("_"*50 + "\n")
+                i+=1
+            except:
+                print("Answer not recognized...please try again...")
+                print("_"*50 + "\n")
     return score
-
-score_now = run_qstnr(phq_prompts)
-fin = run_qstnr(endQ)
-dif = endOpts[fin]
-
-print("Your PHQ-9 score today is: {}\nYou rated your level of difficulty as: {}".format(score_now,dif[4:]))
---
+++ Setup/vars.py
@@ -0,0 +1,41 @@
+from fndef import *
+
+header="Over the past 2 weeks, how often have you been bothered by...\n"
+
+dispOpts = {
+    0:"0 - Not at all",
+    1:"1 - Several days",
+    2:"2 - More than half the days",
+    3:"3 - Nearly every day",
+    "Q":"\n( Enter Q to quit )"
+}
+
+phq_prompts = [
+    Questionnaire(header, "Little interest or pleasure in doing things", dispOpts),
+    Questionnaire(header, "Feeling down, depressed, or hopeless", dispOpts),
+    Questionnaire(header, "Trouble falling asleep or staying asleep, or sleeping too much", dispOpts),
+    Questionnaire(header, "Feeling tired or having little energy", dispOpts),
+    Questionnaire(header, "Poor appetite or overeating", dispOpts),
+    Questionnaire(header, "Feeling bad about yourself; feeling that you are a failure or that you have let yourself or your family down", dispOpts),
+    Questionnaire(header, "Trouble concentrating on things (such as reading the newspaper or watching TV", dispOpts),
+    Questionnaire(header, "Moving or speaking so slowly that other people could have noticed. Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual", dispOpts),
+    Questionnaire(header, "Thoughts that you would be better off dead, or of hurting yourself", dispOpts)
+]
+
+
+endOpts = {
+    0:"0 - Not difficult at all",
+    1:"1 - Somewhat difficult",
+    2:"2 - Very difficult",
+    3:"3 - Extremely difficult",
+    "Q":"\n( Enter Q to quit )"
+}
+
+endQ = [Questionnaire(head="",prompts="If you checked off any problems, how difficult have these problems made it for you to do your work, take care of things at home, or get along with other people?",answers=endOpts)]
+
+
+score_now = run_qstnr(phq_prompts)
+fin = run_qstnr(endQ)
+dif = endOpts[fin]
+
+print("Your PHQ-9 score today is: {}\nYou rated your level of difficulty as: {}".format(score_now,dif[4:]))
